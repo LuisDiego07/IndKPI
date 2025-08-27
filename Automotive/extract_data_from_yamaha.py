a0e5daaa14445bee8fe3e8e1530f5849c44cea05
diego.santos <diego.l.santos@bradescoseguros.com.br>
2025-08-26 22:01:29 -0300

    Release 1 - Automotive

--- Automotive/extract_data_from_yamaha.py
+++ Automotive/extract_data_from_yamaha.py
@@ -1,12 +1,10 @@
+from pathlib import Path
 import pandas as pd
 
-path_crosser_150_2025 = r"C:\Users\diego.oliveira_sense\Downloads\specifications_yamaha_crosser_150_2025.csv"
+# Caminho relativo dentro do projeto
+data_dir = Path(__file__).parent / "data"
+path_crosser_150_2025 = data_dir / "specifications_yamaha_crosser_150_2025.csv"
 
 df_crosser_150_2025 = pd.read_csv(path_crosser_150_2025)
 df_crosser_150_2025["modelo"] = "XTZ 150cc Crosser"
 df_crosser_150_2025["ano"] = "2025"
-
-print(df_crosser_150_2025)
-
-#Loads Data into Postgre SQL
-
