777681b3c1bbbc03d45edc6a9d160c685fbbbca9
diego.santos <diego.l.santos@bradescoseguros.com.br>
2025-08-30 21:42:58 -0300

    Release 1 - Automotive - Motorcycle

--- Automotive/Motorcycle/extract_data_from_yamaha.py
+++ Automotive/Motorcycle/extract_data_from_yamaha.py
@@ -3,9 +3,9 @@ import pandas as pd
 from deltalake.writer import write_deltalake
 
 # Path depends on "data" directory on project
-data_dir = Path(__file__).parent / "data/raw"
-path_crosser_150_2025 = data_dir / "specifications_yamaha_crosser_150_2025.csv"
-path_factor_125_2025 = data_dir / "specifications_yamaha_factor_125_2025.csv"
+data_dir = Path(__file__).parent / "data"
+path_crosser_150_2025 = data_dir / "raw/specifications_yamaha_crosser_150_2025.csv"
+path_factor_125_2025 = data_dir / "raw/specifications_yamaha_factor_125_2025.csv"
 
 df_crosser_150_2025 = pd.read_csv(path_crosser_150_2025)
 df_crosser_150_2025["modelo"] = "XTZ 150cc Crosser"
